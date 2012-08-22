201b24530fa852e7233c832a643ee8261d0c82c5
George Palmer <george.palmer@gmail.com>
2012-08-22 08:47:47 +0100

    Do not load Railtie file

--- lib/symmetric-encryption.rb
+++ lib/symmetric-encryption.rb
@@ -4,13 +4,9 @@ require 'symmetric_encryption/symmetric_encryption'
 require 'symmetric_encryption/reader'
 require 'symmetric_encryption/writer'
 require 'zlib'
-if defined?(Rails)
-  require 'symmetric_encryption/railtie'
-end
 # attr_encrypted and Encrypted validator
 if defined?(ActiveRecord::Base)
   require 'symmetric_encryption/extensions/active_record/base'
-  require 'symmetric_encryption/railties/symmetric_encryption_validator'
 end
 
 # field encryption for Mongoid
